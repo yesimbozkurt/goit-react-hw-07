98d9b9a35a1e7b8f115b1a2a5a2da907503c09ef
yesimbozkurt <yesimsbozkurt@gmail.com>
2025-10-07 01:28:44 +0300

    Refactor ContactsList component: implement loading and error handling

--- src/App.jsx
+++ src/App.jsx
@@ -3,22 +3,11 @@ import './App.css'
 import Form from './components/ContactsForm'
 import SearchBox from './components/SearchBox'
 import ContactsList from './components/ContactsList'
-import { useEffect } from 'react'
-import { fetchContacts } from './redux/operations'
-import { useDispatch, useSelector } from 'react-redux'
-import { LuLoaderCircle } from 'react-icons/lu'
 
 function App() {
-  const dispatch = useDispatch();
-  const { isLoading, error } = useSelector(state => state.contacts);
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
 
   return (
     <>
-      {isLoading && <LuLoaderCircle className='loader' />}
-      {error && <p style={{ color: 'red' }}>Error: {error}</p>}
       <div className="App">
         <h1 className='title'>Phonebook</h1>
         <div className='main-container'>
--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,24 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectFilteredContacts } from '../redux/contactsSlice';
 import Contact from './Contact';
 import css from './ContactsList.module.css';
 import { LuLoaderCircle } from 'react-icons/lu';
+import { fetchContacts } from '../redux/operations';
 
 const ContactsList = () => {
     const contacts = useSelector(selectFilteredContacts);
-    const { isLoading } = useSelector(state => state.contacts);
+    const dispatch = useDispatch();
+    const { isLoading, error } = useSelector(state => state.contacts);
+    useEffect(() => {
+        dispatch(fetchContacts());
+    }, [dispatch]);
 
     return (
         <div className={css.contactsList}>
-            {isLoading && <LuLoaderCircle className='loader' />}
-            <ul className={css.list}>
-                {contacts.map(contact => (
-                    <li key={contact.id} className={css.listItem}>
-                        <Contact contact={contact} />
-                    </li>
-                ))}
-            </ul>
+            {isLoading ? (<LuLoaderCircle className={css.loader} />) : error ? (
+                <p style={{ color: 'red' }}>Error: {error}</p>) : contacts.length > 0 ? (
+                    <ul className={css.list}>
+                        {contacts.map(contact => (
+                            <li key={contact.id} className={css.listItem}>
+                                <Contact contact={contact} />
+                            </li>
+                        ))}
+                    </ul>) : (<p className={css.noContacts}>No contacts found.</p>
+            )}
         </div>
     );
 };
